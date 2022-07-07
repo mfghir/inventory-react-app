a82015b0c1311291384c0db622db2e0f14d13469
miss-f-g <fatemeghafari98@gmail.com>
2022-07-07 12:27:34 +0430

    create category + navbar + product components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Category from "./components/Category";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <div className="bg-slate-800 min-h-screen">
+        <Navbar />
+
+        <div class="container max-w-screen-sm mx-auto p-4">
+<Category />
+        </div>
+
+      </div>
     </div>
   );
 }
